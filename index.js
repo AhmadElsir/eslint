38e1625d49056dfc1a7b5a95f8940beea5aa33ab
AhmedElsir <toto.90019@gmail.com>
2018-08-14 10:17:34 +0200

    initial work!

---
+++ index.js
@@ -0,0 +1,12 @@
+let didShowMessage = true;
+
+const showMessage = () => {
+
+    const message = "I will make the Breakfast today";
+    for (let i = 0; i < 5; i++) {
+        console.log(message);
+        
+    };
+}
+
+showMessage();
